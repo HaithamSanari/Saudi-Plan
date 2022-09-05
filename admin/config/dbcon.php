83edb0de2b7c187f8c1cba3a02c4fc0352cab706
Haitham Sanari <haithamsanari@gmail.com>
2022-09-05 19:52:11 +0300

    Update dbcon.php

--- admin/config/dbcon.php
+++ admin/config/dbcon.php
@@ -1,10 +1,10 @@
 <?php 
 
 
-$host = "us-cdbr-east-06.cleardb.net";
-$username = "b42d1eba35f558";
-$password = "a6a0a82d";
-$database="heroku_0a2abcd4b227aca";
+$host = "localhost";
+$username = "root";
+$password = "";
+$database="saudiPlan";
 
 // Database connection
 $conn = mysqli_connect("$host","$username","$password","$database");
